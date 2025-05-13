6073bf498c9467338d016cea4c05a6afcb72254b
mralg-commits <alex_goh@moe.gov.sg>
2025-05-13 07:34:56 +0000

    fourth commit

--- server/db.js
+++ server/db.js
@@ -1,3 +1,11 @@
 const { Pool } = require('pg');
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+
+// Ensure to set up SSL
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,  // This allows the connection to proceed without verifying the server's certificate (useful for cloud services like Render)
+  },
+});
+
 module.exports = pool;
